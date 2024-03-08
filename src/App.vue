2114b2438b43ab469bedae2a0b0d76e2cbaeb35d
Ernesto Silva <silvaochoa55@gmail.com>
2024-03-07 17:55:01 -0700

    Half time

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span> </h1>
+
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+            <label for="moneda">Moneda:</label>
+            <select id="moneda">
+              
+            </select>
+        </div>
+      </form>
+    </div>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
