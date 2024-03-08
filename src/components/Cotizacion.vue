55dd9db4f7f4a714b907b5acf473181cd2fc4383
Ernesto Silva <silvaochoa55@gmail.com>
2024-03-08 12:53:04 -0700

    Final Commit

---
+++ src/components/Cotizacion.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+    defineProps({
+        cotizacion:{
+            type: Object,
+            required: true
+        }
+    })
+
+</script>
+
+<template>
+      <div class="contenedor-resultado">
+        <h2>Cotización</h2>
+
+        <div class="resultado">
+          <img 
+            :src="'https://cryptocompare.com//' + cotizacion.IMAGEURL" 
+            alt="imagen cripto"
+            >
+          <div>
+              <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
+              <p>Precio mas alto del dia: <span>{{ cotizacion.HIGHDAY }}</span></p>
+              <p>Precio mas bajo del dia: <span>{{ cotizacion.LOWDAY }}</span></p>
+              <p>Variacion  ultimas 2 Horas: <span>{{ cotizacion.CHANGEPTC24HOUR }}%</span></p>
+              <p>Ultima Actualizacion: <span>{{ cotizacion.LASTUPDATE }}</span></p>
+          </div>
+        </div>
+
+      </div>
+</template>
+
